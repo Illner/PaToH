31b3e7079e4121412459497ec6a9e56601c7eedd
Petr Illner <illner3@gmail.com>
2021-04-15 22:14:13 +0200

    None

--- main.py
+++ main.py
@@ -59,9 +59,8 @@ cut_addr = ctypes.addressof(cut_val)
 ok = PATOH_Part(ctypes.byref(patoh_data.params), patoh_data._c, patoh_data._n, patoh_data._nconst, patoh_data.useFixCells, patoh_data._cwghts.ctypes, patoh_data._nwghts.ctypes, patoh_data._xpins.ctypes, patoh_data._pins.ctypes, patoh_data._targetweights.ctypes, patoh_data._partvec.ctypes, patoh_data._partweights.ctypes, cut_addr)
 print(f"PATOH_Part: {ok}")
 
-patoh_data.cut = cut_val
-
 print(f"patoh_data.cut: {patoh_data.cut}")
+print(f"patoh_data.cut_val: {cut_val}")
 print(f"patoh_data._partvec: {patoh_data._partvec}")
 print(f"patoh_data._partweights: {patoh_data._partweights}")
 
